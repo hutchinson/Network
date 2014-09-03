73a7257426d03fbc3a2e119fd5221283bd8b9c65
Dan Hutchinson <d.hutchinson4@googlemail.com>
2014-09-03 20:58:25 +0100

    Setup zmq include/lib path

---
+++ PacketController/PacketListener.h
@@ -0,0 +1,9 @@
+#ifndef __Network__PacketListener__
+#define __Network__PacketListener__
+
+namespace netviz
+{
+  
+}
+
+#endif
--- PacketControllerDriver/main.cpp
+++ PacketControllerDriver/main.cpp
@@ -1,7 +1,10 @@
+#include <zmq.hpp>
 #include <iostream>
 
 int main(int argc, const char * argv[])
 {
+  zmq::context_t zmqContext;
+  
   std::cout << "PacketControllerDriver" << std::endl;
   
   return EXIT_SUCCESS;
